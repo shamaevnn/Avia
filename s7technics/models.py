93bc6b57762ab9da2ae850011ebff735e353808d
nikita <shamaev090299@gmail.com>
2020-05-10 11:29:47 +0300

    new fields in tables

--- s7technics/models.py
+++ s7technics/models.py
@@ -3,32 +3,6 @@ from django.db import models
 # Create your models here.
 
 
-class Hangar(models.Model):
-    HANGARS = (
-        ('DME', 'Домодедово'),
-        ('SVO', 'Шереметьево'),
-        ('VKO', 'Внуково')
-    )
-
-    name = models.CharField(max_length=3, choices=HANGARS, default='DME')
-    width = models.PositiveIntegerField()
-    height = models.PositiveIntegerField()
-    is_filled = models.BooleanField(default=False)
-
-    def __str__(self):
-        return 'Ангар {}'.format(self.name)
-
-
-class AviaCompany(models.Model):
-    name = models.CharField(max_length=64)
-    start_date_TS = models.DateField()
-    end_date_TS = models.DateField()
-    penalty_coef = models.FloatField()
-
-    def __str__(self):
-        return 'Авиакомпания {}'.format(self.name)
-
-
 class AirTypes(models.Model):
     model = models.CharField(max_length=16)
     length = models.FloatField()
@@ -42,9 +16,39 @@ class AirPlane(models.Model):
     bort_number = models.CharField(max_length=64, unique=True)
     plane_type = models.ForeignKey(AirTypes, on_delete=models.PROTECT)
     on_service = models.BooleanField(default=False)
+    x_in_hangar = models.PositiveIntegerField(null=True, blank=True)
+    y_in_hangar = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__(self):
         return 'БН: {}, тип {}'.format(self.bort_number, self.plane_type)
+
+
+class Hangar(models.Model):
+    HANGARS = (
+        ('DME', 'Домодедово'),
+        ('SVO', 'Шереметьево'),
+        ('VKO', 'Внуково')
+    )
+
+    name = models.CharField(max_length=3, choices=HANGARS, default='DME')
+    width = models.PositiveIntegerField()
+    height = models.PositiveIntegerField()
+    is_filled = models.BooleanField(default=False)
+    airplane = models.ForeignKey(AirPlane, on_delete=models.PROTECT, null=True, blank=True)
+    date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return 'Ангар {}'.format(self.name)
+
+
+class AviaCompany(models.Model):
+    name = models.CharField(max_length=64)
+    start_date_TS = models.DateField()
+    end_date_TS = models.DateField()
+    penalty_coef = models.FloatField()
+
+    def __str__(self):
+        return 'Авиакомпания {}'.format(self.name)
 
 
 class TechnicalService(models.Model):
